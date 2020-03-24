967a4c47cd5af26241d40d68f2749fa02d634692
Kunal Taitkar <kunaltaitkar888@gmail.com>
2020-03-24 10:27:46 +0530

    =unaryrpc

--- student/client/client.go
+++ student/client/client.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"grpc-examples/student/studentpb"
+	"io"
 	"log"
 
 	"google.golang.org/grpc"
@@ -18,6 +20,59 @@ func main() {
 
 	studentClient := studentpb.NewStudentServiceClient(conn)
 
-	fmt.Println("%v", studentClient)
+	unaryRequest(studentClient)
+
+	serverStreamingRequest(studentClient)
+
+}
+
+func unaryRequest(client studentpb.StudentServiceClient) {
+
+	payload := &studentpb.RegisterStudentRequest{
+		Student: &studentpb.Student{
+			FirstName: "Kunal",
+			LastName:  "Taitkar",
+		},
+	}
+
+	resp, err := client.RegisterStudent(context.Background(), payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("UNARY RESPONSE:")
+	fmt.Println(resp)
+
+}
+
+func serverStreamingRequest(client studentpb.StudentServiceClient) {
+
+	payload := &studentpb.GetStudentListRequest{
+		CollegeName: "GHRCE",
+	}
+
+	resp, err := client.GetStudents(context.Background(), payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("STEAMING RESPONSE")
+
+	//read stream
+	for {
+
+		response, err := resp.Recv()
+
+		if err == io.EOF {
+			//reached end of stream
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(response)
+	}
 
 }
--- student/server/server.go
+++ student/server/server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"grpc-examples/student/studentpb"
 	"log"
 	"net"
+	"strconv"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -20,11 +23,37 @@ func main() {
 	}
 
 	s := grpc.NewServer()
-	studentpb.RegisterStudentServiceServer(s, &server{})
 
+	studentpb.RegisterStudentServiceServer(s, &server{})
 	err = s.Serve(lis)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+}
+
+//unary request
+func (*server) RegisterStudent(ctx context.Context, req *studentpb.RegisterStudentRequest) (*studentpb.RegisterStudentResponse, error) {
+
+	firstName := req.GetStudent().GetFirstName()
+	lastName := req.GetStudent().GetLastName()
+
+	return &studentpb.RegisterStudentResponse{Result: "hello " + firstName + " " + lastName}, nil
+}
+
+//server streaming
+func (*server) GetStudents(req *studentpb.GetStudentListRequest, stream studentpb.StudentService_GetStudentsServer) error {
+
+	collegeName := req.GetCollegeName()
+
+	for i := 0; i < 10; i++ {
+		response := &studentpb.GetStudentListResponse{
+			CollegeName: collegeName,
+			StudentName: "Student " + strconv.Itoa(i+1),
+		}
+		stream.Send(response)
+		time.Sleep(1 * time.Second)
+	}
+
+	return nil
 }
